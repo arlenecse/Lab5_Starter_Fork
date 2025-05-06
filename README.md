c7da8fb2c03ecaf0ba1f56cee99d326f5b45c8bc
Arlene Garcia <arg030@ucsd.edu>
2025-05-05 18:10:43 -0700

    tests

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Name: Arlene Garcia
 
 Partner Name: Kiara Singh
+
+
+1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+   No, because we cannot test what the recipients received once the message was sent. This can be seen in the cons because we cannot see components interactions.
+
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+    Yes, because unit tests can quickly execuete many example inputs since it is just testing message length validation. We could have tests with less than 80, greater than 80, and equal to 80 characters to test all the bounds and check if the correct output is displayed.
+
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,89 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+//numbers
+test('validnumber1', () => {
+  expect(isPhoneNumber("(619) 457-9788)")).toBe(true);
+});
+
+test('validnumber2', () => {
+  expect(isPhoneNumber("(858)370-2796")).toBe(true);
+});
+
+test('invalidtype', () => {
+  expect(isPhoneNumber(619-457-9788)).toBe(false);
+});
+
+test('invalidnumber', () => {
+  expect(isPhoneNumber('abcde')).toBe(false);
+});
+
+//emails
+test('validemail1', () => {
+  expect(isEmail("arg030@ucsd.edu")).toBe(true);
+});
+
+test('validemail2', () => {
+  expect(isEmail("k6singh@gmail.com")).toBe(true);
+});
+
+
+test('no@sign', () => {
+  expect(isEmail("k6singhgmail.com")).toBe(false);
+});
+
+
+test('wrongending', () => {
+  expect(isEmail("k6singhgmail")).toBe(false);
+});
+
+//password
+test('validpassword1', () => {
+  expect(isStrongPassword("four")).toBe(true);
+});
+
+test('validpassword2', () => {
+  expect(isStrongPassword("Ihopethisworks")).toBe(true);
+});
+
+test('shortpassword', () => {
+  expect(isStrongPassword("bad")).toBe(false);
+});
+
+test('wrongstartchar', () => {
+  expect(isStrongPassword("1hello")).toBe(false);
+});
+
+//date
+test('validdate1', () => {
+  expect(isDate("1/3/2005")).toBe(true);
+});
+
+test('validdate2', () => {
+  expect(isDate("06/12/2005")).toBe(true);
+});
+
+test('invalidnorder', () => {
+  expect(isDate("2005/12/06")).toBe(false);
+});
+
+test('invalidtype', () => {
+  expect(isDate("mar/12/2020")).toBe(false);
+});
+
+//hex
+test('validcolor1', () => {
+  expect(isHexColor("#000000")).toBe(true);
+});
+
+test('validcolor2', () => {
+  expect(isHexColor("#00F020")).toBe(true);
+});
+
+test('missingdigits', () => {
+  expect(isHexColor("0F020")).toBe(false);
+});
+
+test('toomanydigits', () => {
+  expect(isHexColor("#00F0200")).toBe(false);
+});
